63eb5f413a1775ae940cc298c5ae3a419477b577
Milan Košťák <milankostak@seznam.cz>
2019-10-09 19:09:19 +0200

    smazaný starý kód

--- src/streda_17_25_c03/main/AppStart.java
+++ src/streda_17_25_c03/main/AppStart.java
@@ -12,28 +12,6 @@ public class AppStart {
             PGRFWindow window = new PGRFWindow();
             new Controller(window.getRaster());
             window.setVisible(true);
-
-//            window.getRaster().drawPixel(10, 10, Color.YELLOW.getRGB());
-//
-//            window.getRaster().drawPixel(50, 50, 0x00ffff);
-//
-//            window.getRaster().addMouseListener(new MouseAdapter() {
-//                @Override
-//                public void mouseClicked(MouseEvent e) {
-//                    window.getRaster().drawPixel(e.getX(), e.getY(), 0xff0000);
-//                }
-//            });
-//
-//            window.getRaster().addMouseMotionListener(new MouseAdapter() {
-//                @Override
-//                public void mouseMoved(MouseEvent e) {
-//                    window.getRaster().drawPixel(e.getX(), e.getY(), 0xfff000);
-//                }
-//            });
-//
-//            for (int i = 100; i < 300; i++) {
-//                window.getRaster().drawPixel(i, 150, Color.MAGENTA.getRGB());
-//            }
         });
         // https://www.google.com/search?q=SwingUtilities.invokeLater
         // https://www.javamex.com/tutorials/threads/invokelater.shtml
