bb1647aa1dfc9421380a78fb1829ff98e048950c
Milan Košťák <milankostak@seznam.cz>
2019-10-30 19:10:41 +0100

    ComparatorTest.java

---
+++ src/ComparatorTest.java
@@ -0,0 +1,23 @@
+import streda_15_45_c01.model.Point;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ComparatorTest {
+
+    public static void main(String[] args) {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(30, 7));
+        points.add(new Point(1, 2));
+        points.add(new Point(10, -1));
+        points.add(new Point(5, -1));
+
+        points.sort(Comparator.comparingInt(Point::getX));
+
+        for (Point point : points) {
+            System.out.println(point.getX());
+        }
+    }
+
+}
