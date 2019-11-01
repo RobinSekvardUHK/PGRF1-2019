3500a9f0f1e0cbe3800150e97a64c1db238fc7a4
Milan Košťák <milankostak@seznam.cz>
2019-11-01 20:30:35 +0100

    Sort with Comparator.compare() to show the difference

--- src/misc/ComparatorTest.java
+++ src/misc/ComparatorTest.java
@@ -3,6 +3,7 @@ package misc;
 import streda_15_45_c01.model.Point;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -16,6 +17,13 @@ public class ComparatorTest {
         points.add(new Point(5, -1));
 
         points.sort(Comparator.comparingInt(Point::getX));
+        // -> ekvivalentní k následujícímu zápisu
+//        Collections.sort(points, new Comparator<Point>() {
+//            @Override
+//            public int compare(Point o1, Point o2) {
+//                return Integer.compare(o1.getX(), o2.getX());
+//            }
+//        });
 
         for (Point point : points) {
             System.out.println(point.getX());
